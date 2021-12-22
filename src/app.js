f4327486a7040fc083b24ac27c5b5d1891728567
Simon Dunn <simondunn90@gmail.com>
2021-12-22 14:55:53 +0000

    config basic app

--- src/app.js
+++ src/app.js
@@ -1,17 +1,16 @@
+const path = require('path')
 const express = require('express')
-const port = 3000
 const app = express()
+const port = 3000
+const publicDirPath = path.join(__dirname, '../public')
 
-app.get('', (req, res) => {
-  res.send('Hello express!')
-})
-
-app.get('/about', (req, res) => {
-  res.send('About')
-})
+app.use(express.static(publicDirPath))
 
 app.get('/weather', (req, res) => {
-  res.send('<h1> Weather </h1>')
+  res.send({
+    forecast: 'Your weather',
+    location: 'Your town'
+  })
 })
 
 app.listen(port, () => {
